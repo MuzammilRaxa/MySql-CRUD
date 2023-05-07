a6c04ae81a394a02f6e3096e63af77413141c20c
Muzammil Linux <razamuzammil426@gmail.com>
2023-05-07 21:26:39 +0500

    completed

--- backend/index.js
+++ backend/index.js
@@ -1,41 +1,72 @@
 import express from "express"
 import mysql from "mysql"
+import cors from "cors"
 
 const app = express()
 const db = mysql.createConnection({
-    host:"localhost",
+    host: "localhost",
     user: "root",
     password: "razamysql12A@",
     database: "test"
 })
 
 app.use(express.json())
+app.use(cors())
 
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
     res.json("Hello I'm Get result!")
 })
 
-app.get("/books",(req,res)=>{
+app.get("/books", (req, res) => {
     const qr = "SELECT * FROM books"
-    db.query(qr,(err,data)=>{
-        if(err) return res.json(err)
+    db.query(qr, (err, data) => {
+        if (err) return res.json(err)
         return res.json(data)
     })
 })
 
-app.post("/books",(req,res)=>{
-    const qr = "INSERT INTO books (`title`,`desc`,`cover`) VALUES (?)"
+app.post("/books", (req, res) => {
+    const qr = "INSERT INTO books (`title`,`desc`,`price`,`cover`) VALUES (?)"
     const values = [
         req.body.title,
         req.body.desc,
+        req.body.price,
         req.body.cover,
-]
-    db.query(qr,[values],(err,data)=>{
-        if(err) return res.json(err)
+    ]
+    db.query(qr, [values], (err, data) => {
+        if (err) return res.json(err)
         return res.json("Book has been created")
     })
 })
 
-app.listen(8800,()=>{
+
+app.delete("/books/:id", (req, res) => {
+    const bookId = req.params.id
+    const qr = "DELETE FROM books WHERE id =?"
+
+    db.query(qr, [bookId], (err, data) => {
+        if (err) return res.json(err)
+        return res.json("Book has been deleted")
+    })
+})
+
+
+app.put("/books/:id", (req, res) => {
+    const bookId = req.params.id
+    const qr = "UPDATE books SET `title`= ?, `desc`= ?, `price`= ?, `cover` = ? WHERE id = ?";
+
+    const values = [
+        req.body.title,
+        req.body.desc,
+        req.body.price,
+        req.body.cover,
+    ];
+    db.query(qr, values, (err, data) => {
+        if (err) return res.json(err)
+        return res.json(data)
+    });
+});
+
+app.listen(8800, () => {
     console.log('Connected!!')
 })
--- client/src/pages/add.js
+++ client/src/pages/add.js
@@ -1,9 +1,65 @@
-import React from 'react'
+import axios from "axios";
+import Link from "next/link";
+import React from "react";
+import { useState } from "react";
 
-const add = () => {
+const Add = () => {
+  const [book, setBook] = useState({
+    title: "",
+    desc: "",
+    price: null,
+    cover: "",
+  });
+  const [error,setError] = useState(false)
+
+console.log(book)
+  const handleChange = (e) => {
+    setBook((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
+
+  const handleClick = async (e) => {
+    e.preventDefault();
+    try {
+      await axios.post("http://localhost:8800/books", book);
+    } catch (err) {
+      console.log(err);
+      setError(true)
+    }
+  };
+
   return (
-    <div>add</div>
-  )
-}
+    <div className="form">
+      <h1>Add New Book</h1>
+      <input
+        type="text"
+        placeholder="Book title"
+        name="title"
+        onChange={handleChange}
+      />
+      <textarea
+        rows={5}
+        type="text"
+        placeholder="Book desc"
+        name="desc" 
+        onChange={handleChange}
+      />
+      <input
+        type="number"
+        placeholder="Book price"
+        name="price"
+        onChange={handleChange}
+      />
+      <input
+        type="text"
+        placeholder="Book cover"
+        name="cover"
+        onChange={handleChange}
+      />
+      <button onClick={handleClick}>Add</button>
+      {error && "Something went wrong!"}
+      <Link href="/books">See all books</Link>
+    </div>
+  );
+};
 
-export default add
+export default Add;
